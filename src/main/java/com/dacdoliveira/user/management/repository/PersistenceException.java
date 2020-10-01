2abdcc2209bbf17cd181712a8e147a65c11a3d21
doliveira <daco@documentsolutions.com.br>
2020-09-30 21:59:48 -0300

    Cria exception para tratar erros de persistencia de forma controlada

---
+++ src/main/java/com/dacdoliveira/user/management/repository/PersistenceException.java
@@ -0,0 +1,18 @@
+package com.dacdoliveira.user.management.repository;
+
+public class PersistenceException extends RuntimeException
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public PersistenceException(String msg)
+    {
+        super(msg);
+    }
+    
+    
+
+}
