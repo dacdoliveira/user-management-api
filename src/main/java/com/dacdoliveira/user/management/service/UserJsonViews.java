e968840235ae62ab9bb8194eff5d65a963839958
doliveira <daco@documentsolutions.com.br>
2020-09-30 22:01:48 -0300

    Cria estrutura de interface para resolver geração do Json com objetos relacionamentos, evitando recursividade

---
+++ src/main/java/com/dacdoliveira/user/management/service/UserJsonViews.java
@@ -0,0 +1,24 @@
+package com.dacdoliveira.user.management.service;
+
+public class UserJsonViews
+{
+    private UserJsonViews()
+    {
+        
+    }
+    
+    public static interface Normal
+    {
+        
+    }
+    
+    public static interface FromUser extends Normal
+    {
+        
+    }
+    
+    public static interface FromPhone extends Normal
+    {
+        
+    }
+}
